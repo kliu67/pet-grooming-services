fb0ff54913cf701fc252bff5b2fdec00b0ec8c6c
kliu67 <47793951+kliu67@users.noreply.github.com>
2026-02-11 22:47:40 -0500

    add helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,19 @@
+import { validate as isUuid } from "uuid";
+
 export const objectIsEmptyOrNull  = (object)=>{
     
     return false;
+}
+
+export const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export const isNumbersOnly = (str)=> {
+  return /^\d+$/.test(str);
+}
+
+export const isValidUUID = (id) => {
+  return isUuid(id);
 }
