07af8eafea1da9a1899c922004f2fded04b1e7c2
KL <>
2026-02-13 14:07:47 -0500

    lint fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { pool, initDb } from "./db.js";
-import userRoutes from "./routes/users.routes.js"; // ✅
+import userRoutes from "./routes/users.routes.js";
 import serviceRoutes from "./routes/services.routes.js"
 
 
@@ -29,59 +29,59 @@ if (process.env.NODE_ENV !== "test") {
 // -------- CRUD ROUTES --------
 
 // Create
-app.post("/api/items", async (req, res) => {
-  const { name, description } = req.body;
+// app.post("/api/items", async (req, res) => {
+//   const { name, description } = req.body;
 
-  const result = await pool.query(
-    "INSERT INTO items (name, description) VALUES ($1, $2) RETURNING *",
-    [name, description]
-  );
+//   const result = await pool.query(
+//     "INSERT INTO items (name, description) VALUES ($1, $2) RETURNING *",
+//     [name, description]
+//   );
 
-  res.json(result.rows[0]);
-});
+//   res.json(result.rows[0]);
+// });
 
 
 // Read all
-app.get("/api/items", async (req, res) => {
-  const result = await pool.query("SELECT * FROM items ORDER BY id DESC");
-  res.json(result.rows);
-});
+// app.get("/api/items", async (req, res) => {
+//   const result = await pool.query("SELECT * FROM items ORDER BY id DESC");
+//   res.json(result.rows);
+// });
 
 // Read one
-app.get("/api/items/:id", async (req, res) => {
-  const result = await pool.query(
-    "SELECT * FROM items WHERE id = $1",
-    [req.params.id]
-  );
+// app.get("/api/items/:id", async (req, res) => {
+//   const result = await pool.query(
+//     "SELECT * FROM items WHERE id = $1",
+//     [req.params.id]
+//   );
 
-  if (!result.rows.length) {
-    return res.status(404).json({ error: "Not found" });
-  }
+//   if (!result.rows.length) {
+//     return res.status(404).json({ error: "Not found" });
+//   }
 
-  res.json(result.rows[0]);
-});
+//   res.json(result.rows[0]);
+// });
 
 // Update
-app.put("/api/items/:id", async (req, res) => {
-  const { name, description } = req.body;
+// app.put("/api/items/:id", async (req, res) => {
+//   const { name, description } = req.body;
 
-  const result = await pool.query(
-    `
-    UPDATE items
-    SET name = $1, description = $2
-    WHERE id = $3
-    RETURNING *
-    `,
-    [name, description, req.params.id]
-  );
-  res.json(result.rows[0]);
-});
+//   const result = await pool.query(
+//     `
+//     UPDATE items
+//     SET name = $1, description = $2
+//     WHERE id = $3
+//     RETURNING *
+//     `,
+//     [name, description, req.params.id]
+//   );
+//   res.json(result.rows[0]);
+// });
 
 // Delete
-app.delete("/api/items/:id", async (req, res) => {
-  await pool.query("DELETE FROM items WHERE id = $1", [req.params.id]);
-  res.json({ deleted: req.params.id });
-});
+// app.delete("/api/items/:id", async (req, res) => {
+//   await pool.query("DELETE FROM items WHERE id = $1", [req.params.id]);
+//   res.json({ deleted: req.params.id });
+// });
 
 
 app.get("/api/health", (req, res) => {
--- routes/services.routes.js
+++ routes/services.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import * as Service from "../models/service.model.js";
-import {isNumbersOnly, isValidEmail, isValidId, isValidPrice} from "../utils/helpers.js";
+import { isNumbersOnly, isValidEmail, isValidId, isValidPrice } from "../utils/helpers.js";
 import { validateServiceInput } from "../validators/service.validator.js";
 const PSQL_ERRORS = {
-  UNIQUE_VIOLATION:{
-    CODE:'23505',
-  },
+    UNIQUE_VIOLATION: {
+        CODE: '23505',
+    },
 }
 
 const router = express.Router();
@@ -16,43 +16,43 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    try{
+    try {
         const id = validateNumericId(req.params.id);
         const service = await Service.findById(req.params.id);
-        if(!service) return res.status(404).json({error: "service not found"});
+        if (!service) return res.status(404).json({ error: "service not found" });
         res.json(service);
     }
 
-    catch(err){
-        if(!id){
-        return res.status(400).json({error: "id is invalid"});
+    catch (err) {
+        if (!id) {
+            return res.status(400).json({ error: "id is invalid" });
         }
         console.log(err);
     }
 })
 
 router.post("/", async (req, res) => {
-    try{
+    try {
         validateServiceInput(req.body);
         const service = await Service.create(req.body);
         res.status(201).join(service);
     }
 
-    catch(err) {
+    catch (err) {
         if (err.details) {
             return res.status(400).json({ errors: err.details });
         }
-        else{
+        else {
             res.status(500).json({ error: "Internal server error" });
         }
     }
 })
 
 router.put("/", async (req, res) => {
-    if(!id){
-        return res.status(400).json({error: "id is invalid"});
+    if (!id) {
+        return res.status(400).json({ error: "id is invalid" });
     }
-    try{
+    try {
         const id = validateNumericId(req.params.id);
         validateServiceInput(req.body);
         const service = await Service.update(id, req.body);
@@ -60,10 +60,32 @@ router.put("/", async (req, res) => {
     }
 
     catch (err) {
-        if(!id){
-            return res.status(400).json({error: "id is invalid"});
+        if (!id) {
+            return res.status(400).json({ error: "id is invalid" });
         }
-        if(err.message) res.status(400).json({ error: err.message });
-        else res.status(500).json({ error: "Internal server error"});
+        if (err.message) res.status(400).json({ error: err.message });
+        else res.status(500).json({ error: "Internal server error" });
     }
 })
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = validateNumericId(req.params.id);
+        const ok = await Service.remove(id);
+        if (!ok) {
+            res.status(404).json({ error: "Not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+    }
+
+    catch (err) {
+        if (!id) {
+            return res.status(400).json({ error: "id is invalid" });
+        }
+        console.log(err);
+        res.status(400).json({ error: `Invalid request: ${err?.message}` });
+    }
+
+});
+
+export default router;
