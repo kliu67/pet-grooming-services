944e8bd84deb70316f1ae25eebd7dcedf36ed29f
KL <>
2026-02-13 16:13:41 -0500

    refactor users schema to separate full_name into first_name and last_name

--- db.js
+++ db.js
@@ -47,7 +47,8 @@ export async function initDb() {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    full_name VARCHAR(60) NOT NULL,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
     email TEXT UNIQUE,
     phone TEXT UNIQUE NOT NULL,
     description TEXT,
