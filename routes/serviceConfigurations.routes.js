49f995985ca3ef46791dda85b510715c06601db1
KL <>
2026-03-17 14:35:59 -0400

    fix get all service config outes

--- routes/serviceConfigurations.routes.js
+++ routes/serviceConfigurations.routes.js
@@ -11,10 +11,10 @@ import {
 const serviceConfigrationRoutes = express.Router();
 
 /* Get single configuration by composite key */
-serviceConfigrationRoutes.get("/", getConfiguration);
+// serviceConfigrationRoutes.get("/", getConfiguration);
 
 /* List all configurations */
-serviceConfigrationRoutes.get("/all", getAllConfigurations);
+serviceConfigrationRoutes.get("/", getAllConfigurations);
 
 /* List all configurations for a service */
 serviceConfigrationRoutes.get("/service/:serviceId", getConfigurationsByService);
